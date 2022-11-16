86d060fbca00ea6919f062b7de1f73f999b9575d
Shi Warai <zhuravlev.p.r@edu.mirea.ru>
2022-11-16 21:04:46 +0300

    Fix: change driver init

--- tests/tests/conftest.py
+++ tests/tests/conftest.py
@@ -5,27 +5,23 @@ from webdriver_manager.chrome import ChromeDriverManager, ChromeType
 from tests.ui.pages_fixtures import *
 
 
-@pytest.fixture()
-def config():
-    return {'web_url': "http://localhost/", 'api_url': "http://localhost:8080/"}
+def pytest_addoption(parser):
+    parser.addoption('--browser', default=r'..\chromedriver.exe')
+
+
+@pytest.fixture(scope='session')
+def config(request):
+    browser = request.config.getoption('--browser')
+    return {'browser': browser, 'web_url': "http://localhost/", 'api_url': "http://localhost:8080/"}
 
 
 @pytest.fixture(scope='function')
 def driver(config):
-    options = Options()
-
-    browser_name = config['browser']
-    if browser_name == 'chrome':
-        # There I haven't used a webdriver, because now it's working on Linux with Chromium correctly
-        # options.add_argument("--remote-debugging-port=9222")  # Doesn't work without it on Ubuntu
-        manager = ChromeDriverManager(version='latest', chrome_type=ChromeType.GOOGLE)
-        browser = webdriver.Chrome(executable_path = manager.install(), options = options)
-    else:
-        raise RuntimeError(f'Unsupported browser: {browser_name}')
-
+    # change for another system
+    browser = webdriver.Chrome(executable_path=config['browser'])
     browser.maximize_window()
 
-    browser.get(config['default_url'])
+    browser.get(config['web_url'])
 
     yield browser
 
