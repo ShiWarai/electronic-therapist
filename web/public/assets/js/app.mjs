dbbed7dfcc262b93103661c8d72b70e563e43842
Shi Warai <zhuravlev.p.r@edu.mirea.ru>
2022-11-17 21:02:55 +0300

    Feat: add UI
    Also change local port

--- web/public/assets/js/app.mjs
+++ web/public/assets/js/app.mjs
@@ -1,18 +1,35 @@
-import {fetch, set, options} from './api.mjs'
+import {get} from "./api.mjs"
+import {ExaminationComponent} from "./examination.mjs"
+import {ResultComponent} from "./result.mjs"
 
 
 const ElectronicTherapistApp = {
     data() {
         return {
+            current_page: null,
+            questions_and_answers: []
         }
     },
     methods: {
+        start_examination() {
+            this.current_page = 'examination'
+        },
+        show_result(questions_and_answers) {
+            this.questions_and_answers = questions_and_answers
+            this.current_page = 'result'
+        },
+        restart() {
+            this.questions_and_answers = null
+            this.current_page = 'welcome'
+        }
     },
     mounted () {
-        // Check connection
+        this.current_page = 'welcome'
     },
     components: 
     {
+        ExaminationComponent,
+        ResultComponent
     }
 }
 
